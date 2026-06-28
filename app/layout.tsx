94621880778cc976984dc2cbf34d3fc6cfd40824
meccar <tung14hn@gmail.com>
2026-06-28 23:16:31 +0700

    feat: implemented base color

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "TAF Viet",
+  description: "Kế toán thuế TAF Việt",
 };
 
 const geistSans = Geist({
